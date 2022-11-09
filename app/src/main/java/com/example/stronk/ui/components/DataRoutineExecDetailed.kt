46b64f9c46e041c3fd2eb83ac72b679bced80e2f
Mauro Báez <mabaez@itba.edu.ar>
2022-11-09 13:47:27 -0300

    Update DataRoutineExecDetailed.kt

--- app/src/main/java/com/example/stronk/ui/components/DataRoutineExecDetailed.kt
+++ app/src/main/java/com/example/stronk/ui/components/DataRoutineExecDetailed.kt
@@ -1,14 +1,21 @@
 package com.example.stronk.ui.components
 
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,7 +42,8 @@ fun TitleAndSubtitle(MainText: String, SecondaryText: String)
                 fontWeight = FontWeight(600)
             )
             // TODO: Cómo centro el divider?
-            Divider(color = Color.Black, thickness = 2.dp, modifier = Modifier.fillMaxWidth(0.95F))
+            Divider(color = Color.Black, thickness = 2.dp,
+                modifier = Modifier.fillMaxWidth(0.95F))
             Text(
                 text = SecondaryText,
                 textAlign = TextAlign.Center,
@@ -50,13 +58,71 @@ fun TitleAndSubtitle(MainText: String, SecondaryText: String)
     }
 }
 
+@Composable
+fun InfoCycle(currentCycle: String, nextExer: String)
+{
+    Column(modifier = Modifier
+        .padding(10.dp)) {
+        /*Row {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(3.dp)
+                    .background(color = MaterialTheme.colors.primary, CircleShape)
+                    .padding(10.dp),
+                text = "Ciclo Actual: $currentCycle",
+                textAlign = TextAlign.Start,
+                color = MaterialTheme.colors.secondary,
+            )
+        }*/
+        // TODO: que los textos como currentCycle se alineen a la derecha
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(3.dp)
+            .background(color = MaterialTheme.colors.primary, CircleShape)
+            .padding(10.dp),) {
+
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = MaterialTheme.colors.secondary, fontWeight = FontWeight.Normal)) {
+                        append("Ciclo Actual: ") // falta traducirlo al inglés
+                    }
+                    withStyle(style = SpanStyle(color = MaterialTheme.colors.secondary, fontWeight = FontWeight.Black)) {
+                        append(currentCycle)
+                    }
+                }
+            )
+        }
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(3.dp)
+            .background(color = MaterialTheme.colors.primary, CircleShape)
+            .padding(10.dp),) {
+
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = MaterialTheme.colors.secondary, fontWeight = FontWeight.Normal)) {
+                        append("Próximo Ejercicio: ") // falta traducirlo al inglés
+                    }
+                    withStyle(style = SpanStyle(color = MaterialTheme.colors.secondary, fontWeight = FontWeight.Black)) {
+                        append(nextExer)
+                    }
+                }
+
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
-fun MyPreview(MainText: String = "asasdas", SecondaryText: String = "sdfsdfsf")
+fun MyPreview(MainText: String = "Abdominales (1/3)", SecondaryText: String = "Descanso")
 {
     StronkTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-            TitleAndSubtitle(MainText = MainText, SecondaryText = SecondaryText)
+            //TitleAndSubtitle(MainText = MainText, SecondaryText = SecondaryText)
+            InfoCycle(currentCycle = MainText, nextExer = SecondaryText)
         }
     }
 }
